3166ed084bbe58af079bf4752f2716ffec7f0627
utooley <utooley@gmail.com>
2019-01-14 19:26:00 -0500

    added mriqc commands

---
+++ code/ppc/mriqc_cmd.sh
@@ -0,0 +1,18 @@
+MACKEY_HOME=/data/picsl/mackey_group/
+#BIDS_folder=/data/picsl/mackey_group/BPD/niftis
+BIDS_folder=/data/picsl/mackey_group/BPD/CBPD_bids
+working_dir=/data/picsl/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/data/mriqc_working
+subject=${1}
+tools_dir=${MACKEY_HOME}/tools/singularity
+output_dir=${BIDS_folder}/derivatives/mriqc
+
+unset PYTHONPATH;
+singularity run --cleanenv -B ${BIDS_folder}:/mnt,${working_dir}:/tmp ${tools_dir}/mriqc-0.14.2.simg \
+/mnt/ /mnt/derivatives/mriqc \
+participant \
+-w /tmp \
+--participant_label ${subject} \
+--fd_thres 0.5 \
+--no-sub \
+
+#must bind to a folder that already exists in the container, and must point to data dir not subject dir
