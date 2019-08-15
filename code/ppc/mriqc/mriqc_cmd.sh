3436c7552c7a9a24903d0ac21ff15580442f18a4
utooley <utooley@gmail.com>
2019-08-15 17:04:07 -0400

    up to date

--- code/ppc/mriqc/mriqc_cmd.sh
+++ code/ppc/mriqc/mriqc_cmd.sh
@@ -4,16 +4,17 @@ BIDS_folder=/data/picsl/mackey_group/BPD/CBPD_bids
 working_dir=/data/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/data/mriqc_working
 subject=${1}
 tools_dir=${MACKEY_HOME}/tools/singularity
-output_dir=${BIDS_folder}/derivatives/mriqc_fd_.5_mm
+output_dir=${BIDS_folder}/derivatives/mriqc_fd_2_mm
 echo $subject
 
 unset PYTHONPATH;
 singularity run --cleanenv -B ${BIDS_folder}:/mnt ${tools_dir}/mriqc-0.14.2.simg \
-/mnt/ /mnt/derivatives/mriqc_fd_.5_mm \
+/mnt/ /mnt/derivatives/mriqc_fd_2_mm \
 participant \
 -w /tmp \
 --participant_label ${subject} \
---fd_thres .5 \
+--fd_thres 2 \
 --no-sub \
+--n_procs 10 \
 
 #must bind to a folder that already exists in the container, and must point to data dir not subject dir
--- code/ppc/xcpParallel.sh
+++ code/ppc/xcpParallel.sh
@@ -2,12 +2,12 @@
 #$ -q all.q,basic.q,himem.q,gpu.q
 #$ -l h_vmem=19.1G,s_vmem=19.0G
 #$ -o /data/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/output/qsub_output
-#$ -t 1-98
+#$ -t 1-99
 
 # Adjust these so they work on your system
 SNGL=/share/apps/singularity/2.5.1/bin/singularity
-SIMG=/data/joy/BBL/applications/bids_apps/xcpEngine.simg
-FULL_COHORT=/data/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/data/subjectLists/n75_cohort_mult_runs_usable_t1_rest_1mm_outliers_10_2mm_80119.csv
+SIMG=/data/picsl/mackey_group/tools/singularity/xcpEngine.simg
+FULL_COHORT=/data/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/data/subjectLists/n76_cohort_mult_runs_usable_t1_rest_1mm_outliers_10_2mm_80119.csv
 
 # Create a temp cohort file with 1 line
 HEADER=$(head -n 1 $FULL_COHORT)
@@ -19,7 +19,7 @@ echo $LINE >> $TEMP_COHORT
 
 $SNGL run --cleanenv -B /data:/mnt $SIMG \
   -c /mnt${TEMP_COHORT#/data} \
-  -d /mnt/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/code/ppc/fc-36p-gsr-meancsfwm-censor-contig5-fd0.5-dv1.75-dropvols.dsn \
-  -o /mnt/picsl/mackey_group//BPD/CBPD_bids/derivatives/xcpEngine_gsr_censor_5contig_fd0.5dvars1.75_drpvls_2 \
+  -d /mnt/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/code/ppc/fc-36p-gsr-meancsfwm-censor-contig5-fd1.25-dv2-dropvols.dsn \
+  -o /mnt/picsl/mackey_group//BPD/CBPD_bids/derivatives/xcpEngine_gsr_censor_5contig_fd1.25dvars2_drpvls \
   -i $TMPDIR
 
