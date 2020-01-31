bc5882047a5976fad5795e25cc72e64c5eadebab
Ursula Tooley <utooley@gmail.com>
2020-01-31 11:42:56 -0800

    started modifying long timepoints script

--- code/analysis/longitudinal_timepoints.R
+++ code/analysis/longitudinal_timepoints.R
@@ -4,12 +4,12 @@ library(ggplot2)
 data_dir="~/Documents/projects/in_progress/within_between_network_conn_CBPD/data/subjectData/"
 data_maintenance_day_dir="~/Box/Mackey_Lab/CBPD (825656)/Data/Data Maintenance Day/CURRENT_2019-09-Data/"
 
-alldata=read.csv(paste0(data_maintenance_day_dir,"CBPD_data_DMD_2019.10.17.csv"))
-long_t2_time_gaps_only=read.csv(paste0(data_dir,"long_t2_data_gaps.csv"), )
+alldata=read.csv(paste0(data_dir,"CBPD_data_redcapexport_2020.1.31.csv"))
+long_t2_time_gaps_only=read.csv(paste0(data_dir,"long_t2_data_gaps_1.31.20.csv"), )
 
 #make new id to match on, match the longitudinal time gaps from one file to the baseline data in the other
-long_t2_time_gaps_only$idlong <- as.character(long_t2_time_gaps_only$id)
-long_t2_time_gaps_only$record_id <- sub(pattern = "_2", "",long_t2_time_gaps_only$idlong)
+long_t2_time_gaps_only$idlong <- as.character(long_t2_time_gaps_only$record_id)
+long_t2_time_gaps_only$record_id <- sub(pattern = "_[23]", "",long_t2_time_gaps_only$idlong)
 long_t2_time_gaps_only$record_id
 
 all <- left_join(long_t2_time_gaps_only, alldata, by= "record_id")
@@ -25,8 +25,11 @@ all$gapbetweenmnths <- as.numeric(difftime(all$date_behav.x, all$date_behav.y)/3
 #age at t1
 hist(all$age_behav.y, main="Age at T1", col="lightblue")
 summary(all$age_behav.y)
-#age at t2
+#age at longitudinal
 hist(all$age_behav.x, main="Age at T2", col="blue")
+summary(all$age_behav.x)
+#age at t3
+hist(all$age_behav., main="Age at T2", col="blue")
 summary(all$age_behav.x)
 
 #gap between them
