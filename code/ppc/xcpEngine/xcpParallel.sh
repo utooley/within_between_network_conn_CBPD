a008ec392234c35ae0c82e15985aa442e1a37cc5
utooley <utooley@gmail.com>
2021-03-24 17:10:13 -0400

    changes to xcp scripts to run on CUBIC

--- code/ppc/xcpEngine/xcpParallel.sh
+++ code/ppc/xcpEngine/xcpParallel.sh
@@ -1,13 +1,12 @@
 #$ -j y
-#$ -q all.q,gpu.q,himem.q
-#$ -l h_vmem=90.1G,s_vmem=90.0G
-#$ -o /data/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/output/qsub_output
-#$ -t 1-2
+#$ -l h_vmem=95.1G,s_vmem=95.0G
+#$ -o /cbica/projects/cbpd_main_data/qsub_output
+#$ -t 1-3
 
 # Adjust these so they work on your system
-SNGL=/share/apps/singularity/2.5.1/bin/singularity
-SIMG=/data/picsl/mackey_group/tools/singularity/xcpEngine-100219.simg
-FULL_COHORT=/data/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/data/subjectLists/nxxx_cleanup_for_xcpEngine.csv
+SNGL=/usr/bin/singularity
+SIMG=/cbica/projects/cbpd_main_data/tools/singularity/xcpEngine-100219.simg
+FULL_COHORT=/cbica/home/tooleyu/projects/in_progress/within_between_network_conn_CBPD/data/subjectLists/n3_anne_cohort_piper_xcpEngine.csv
 
 # Create a temp cohort file with 1 line
 HEADER=$(head -n 1 $FULL_COHORT)
@@ -18,10 +17,9 @@ echo $HEADER > $TEMP_COHORT
 echo $LINE >> $TEMP_COHORT
 
 echo 'temporary directory is ' $TMPDIR
-$SNGL run --cleanenv -B /data:/mnt,$TMPDIR:/tmp $SIMG \
-  -c /mnt${TEMP_COHORT#/data} \
-  -d /mnt/jux/mackey_group/Ursula/projects/in_progress/within_between_network_conn_CBPD/code/ppc/fc-36p-nogsr-meancsfwm-spkreg-dropvols.dsn \
-  -o /mnt/picsl/mackey_group//BPD/CBPD_bids/derivatives/xcpEngine_nogsr_spkreg_fd1.25dvars2_drpvls \
-  -i /mnt/picsl/mackey_group//BPD/CBPD_bids/derivatives/fmriprep_wd \
+$SNGL run --cleanenv --env R_PROFILE_USER=usr,R_ENVIRON_USER=usr -B $TMPDIR:/tmp $SIMG \
+  -c ${TEMP_COHORT} \
+  -d /cbica/home/tooleyu/projects/in_progress/within_between_network_conn_CBPD/code/ppc/fc-36p-nogsr-meancsfwm-spkreg-dropvols.dsn \
+  -o /cbica/projects/cbpd_main_data/CBPD_bids_crosssectional/derivatives/xcpEngine_nogsr_spkreg_fd1.25dvars2_drpvls \
+  -i /cbica/projects/cbpd_main_data/CBPD_bids_crosssectional/derivatives/xcpengine_wd_2 \
   -t 2
-
