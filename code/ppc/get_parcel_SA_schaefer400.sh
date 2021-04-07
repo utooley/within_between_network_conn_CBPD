59889221a7083401be7baa7b286ea6e985489483
Ursula Tooley <utooley@gmail.com>
2021-04-07 14:49:43 -0400

    spin tests added

--- code/ppc/get_parcel_SA_schaefer400.sh
+++ code/ppc/get_parcel_SA_schaefer400.sh
@@ -1,4 +1,4 @@
-export SUBJECTS_DIR=/cbica/projects/cbpd_main_data/CBPD_bids_crosssectional/derivatives/freesurfer
+export SUBJECTS_DIR=/cbica/projects/cbpd_main_data/CBPD_bids/derivatives/freesurfer_edits_t1
 
 annot_file_lh=/cbica/projects/cbpd_main_data/tools/CBIG/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/FreeSurfer5.3/fsaverage/label/lh.Schaefer2018_400Parcels_7Networks_order.annot
 annot_file_rh=/cbica/projects/cbpd_main_data/tools/CBIG/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/FreeSurfer5.3/fsaverage/label/rh.Schaefer2018_400Parcels_7Networks_order.annot
@@ -17,11 +17,19 @@ do
 annot_file=${SUBJECTS_DIR}/${subjid}/label/${hemi}.Schaefer2018_400Parcels_7Networks_order.annot
 
 #get thickness and surface area
-mri_segstats --annot ${subjid} ${hemi} ${annot_file} --i $SUBJECTS_DIR/${subjid}/surf/${hemi}.thickness --accumulate --snr --sum ${SUBJECTS_DIR}/${subjid}/stats/${hemi}.schaefer400_7nets.thickness.stats
+mri_segstats --annot ${subjid} ${hemi} ${annot_file} --i $SUBJECTS_DIR/${subjid}/surf/${hemi}.thickness --snr --sum ${SUBJECTS_DIR}/${subjid}/stats/${hemi}.schaefer400_7nets.thickness.stats
 mri_segstats --annot ${subjid} ${hemi} ${annot_file} --i $SUBJECTS_DIR/${subjid}/surf/${hemi}.area --accumulate --snr --sum ${SUBJECTS_DIR}/${subjid}/stats/${hemi}.schaefer400_7nets.surfarea.stats
 
+#when doing area or volume, use --accumulate flag to mri_segstats to get the total area or volume. Don't do with CT to get mean.
 done
 done
 
 subjlist=`find ${SUBJECTS_DIR} -iname "sub-CBPD*" -exec basename {} \;`
-aparcstats2table --subjects ${subjlist} --hemi lh --parc schaefer400_7nets.surfarea --meas area --tablefile schaefer400_lh_surfarea_stats.txt --skip
+declare -a array=(lh
+rh)
+for hemi in "${array[@]}";
+do
+aparcstats2table --subjects ${subjlist} --hemi ${hemi} --parc schaefer400_7nets.surfarea --meas area --tablefile schaefer400_${hemi}_surfarea_stats.txt --skip
+asegstats2table --subjects ${subjlist} --statsfile ${hemi}.schaefer400_7nets.thickness.stats --meas mean --tablefile schaefer400_${hemi}_thickness_stats.txt --skip
+#for some reason aparcstats2table didn't work for thickness, so use asegstats2table
+done
